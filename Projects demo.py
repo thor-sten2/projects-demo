e954ac3099aaf919eefbd26e8921c655a4d1ce0c
thorsten.jacobs@databricks.com <thorsten.jacobs@databricks.com>
2021-01-22 00:09:10 +0000

    Initial commit

---
+++ Projects demo.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Markdown cell
+# MAGIC 
+# MAGIC Some text
+
+# COMMAND ----------
+
+# Python code
+print('Hello projects!')
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC // Scala code
+# MAGIC println("Hello projects!")
+
+# COMMAND ----------
+
+# MAGIC %r
+# MAGIC # R code
+# MAGIC print('Hello projects!')
